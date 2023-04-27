4a60da66cd0d1ee7810e4b85b3a5579293ffd767
Jamie Harget-Dash <jamie.hargetdash@team.bumble.com>
2023-04-27 13:25:26 +0100

    fix string interpolation for php 8.2

--- src/REST/Section/Version.php
+++ src/REST/Section/Version.php
@@ -86,7 +86,7 @@ class Version extends Section
             $parameters['moveAffectedIssuesTo'] = $move_affected_to;
         }
 
-        $this->Jira->delete("/version/${id}", $parameters);
+        $this->Jira->delete("/version/{$id}", $parameters);
     }
 
     /**
@@ -112,7 +112,7 @@ class Version extends Section
             $parameters['after'] = $after;
         }
 
-        return $this->Jira->post("/version/${id}/move", $parameters);
+        return $this->Jira->post("/version/{$id}/move", $parameters);
     }
 
     /**
@@ -128,6 +128,6 @@ class Version extends Section
      */
     public function get(int $id) : \stdClass
     {
-        return $this->Jira->get("/version/${id}");
+        return $this->Jira->get("/version/{$id}");
     }
 }
